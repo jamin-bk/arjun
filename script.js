58a613447651cf581c29f9a24a6e09fb75a2426f
jamin-bk <103749903+jamin-bk@users.noreply.github.com>
2022-07-07 21:37:13 +0600

    pre-final9

--- script.js
+++ script.js
@@ -21,28 +21,34 @@ function CheckedFun(){
     let CourseBlockDiv2 = document.getElementById('course-block-div2');
     let CourseBlockDiv3 = document.getElementById('course-block-div3');
     let CourseBlockDiv4 = document.getElementById('course-block-div4');
-    if(BorderStyle1.checked){
-        CourseBlockDiv1.style.borderRadius =25 + 'px';
-        CourseBlockDiv1.style.WebkitBorderRadius = 25 + 'px';
+    const MediaFloat = window.matchMedia('(max-width:460px)');
+    if(MediaFloat.matches){
+        if(BorderStyle1.checked){
+            CourseBlockDiv1.style.borderRadius =25 + 'px';
+            CourseBlockDiv1.style.WebkitBorderRadius = 25 + 'px';
+        }else{
+            CourseBlockDiv1.style.borderRadius = 50 + 'px';
+        }
+        if(BorderStyle2.checked){
+            CourseBlockDiv2.style.borderRadius =25 + 'px';
+            CourseBlockDiv2.style.WebkitBorderRadius = 25 + 'px';
+        }else{
+            CourseBlockDiv2.style.borderRadius = 50 + 'px';
+        }
+        if(BorderStyle3.checked){
+            CourseBlockDiv3.style.borderRadius = '25px';
+            CourseBlockDiv3.style.WebkitBorderRadius = '25px';
+        }else{
+            CourseBlockDiv3.style.borderRadius = '50px';
+        }
+        if(BorderStyle4.checked){
+            CourseBlockDiv4.style.borderRadius = '25px';
+            CourseBlockDiv4.style.WebkitBorderRadius = '25px';
+        }else{
+            CourseBlockDiv4.style.borderRadius = '50px';
+        }
     }else{
-        CourseBlockDiv1.style.borderRadius = 50 + 'px';
+        document.querySelectorAll('.course-block-div').style.borderRadius = '50px';
     }
-    if(BorderStyle2.checked){
-        CourseBlockDiv2.style.borderRadius =25 + 'px';
-        CourseBlockDiv2.style.WebkitBorderRadius = 25 + 'px';
-    }else{
-        CourseBlockDiv2.style.borderRadius = 50 + 'px';
-    }
-    if(BorderStyle3.checked){
-        CourseBlockDiv3.style.borderRadius = '25px';
-        CourseBlockDiv3.style.WebkitBorderRadius = '25px';
-    }else{
-        CourseBlockDiv3.style.borderRadius = '50px';
-    }
-    if(BorderStyle4.checked){
-        CourseBlockDiv4.style.borderRadius = '25px';
-        CourseBlockDiv4.style.WebkitBorderRadius = '25px';
-    }else{
-        CourseBlockDiv4.style.borderRadius = '50px';
-    }
+    
 }
